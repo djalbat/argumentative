16faee09a269bd2cc73ef1f707deb08e39a938aa
James Smith <james.smith@djalbat.com>
2024-03-03 19:25:33 +0000

    Propagated.

--- bin/utilities/case.js
+++ bin/utilities/case.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function snakeCaseToCamelCase(string) {
-  return string.replace(/-(.)/g, (match, character) => (character) => {
+  return string.replace(/-(.)/g, (match, character) => {
     const upperCaseCharacter = character.toUpperCase();
 
     return upperCaseCharacter;

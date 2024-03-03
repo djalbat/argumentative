63381afb2ff6e07dd161740931eee597105736ce
James Smith <james.smith@djalbat.com>
2024-03-03 19:24:13 +0000

    A tiny, cosmetic fiz.

--- bin/utilities/case.js
+++ bin/utilities/case.js
@@ -1,7 +1,11 @@
 "use strict";
 
 function snakeCaseToCamelCase(string) {
-  return string.replace(/-(.)/g, (match, character) => character.toUpperCase());
+  return string.replace(/-(.)/g, (match, character) => (character) => {
+    const upperCaseCharacter = character.toUpperCase();
+
+    return upperCaseCharacter;
+  });
 }
 
 module.exports = {
